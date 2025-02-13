755c38babb989da37c252270a6c8799bbb7ae1b3
aissaoui yamina <55004651+milena-cmd@users.noreply.github.com>
2025-02-13 10:16:07 +0100

    train.py

--- train.py
+++ train.py
@@ -1,18 +1,19 @@
 # src/train.py
 
-import argparse
 import os
+import argparse
 
 def main(model_choice):
     if model_choice == 'bert':
         from bert_model import train_bert_model
-        data_file = os.path.join(os.getcwd(), 'data', 'processed', 'oula_processed.csv')
+        # Use Amazon Books Reviews processed file
+        data_file = os.path.join(os.getcwd(), 'data', 'processed', 'amazon_books_processed.csv')
         model_save_path = os.path.join(os.getcwd(), 'results', 'bert_model')
         os.makedirs(model_save_path, exist_ok=True)
         train_bert_model(data_file, model_save_path)
     elif model_choice == 'rnn':
         from rnn_lstm_model import train_rnn_lstm_model
-        data_file = os.path.join(os.getcwd(), 'data', 'processed', 'oula_processed.csv')
+        data_file = os.path.join(os.getcwd(), 'data', 'processed', 'amazon_books_processed.csv')
         model_save_path = os.path.join(os.getcwd(), 'results', 'rnn_lstm_model')
         os.makedirs(model_save_path, exist_ok=True)
         train_rnn_lstm_model(data_file, model_save_path)
@@ -20,8 +21,8 @@ def main(model_choice):
         print('Invalid model choice. Use \"bert\" or \"rnn\".')
 
 if __name__ == '__main__':
-    import argparse
     parser = argparse.ArgumentParser(description='Train BERT or RNN-LSTM model.')
     parser.add_argument('--model', type=str, required=True, help='Model to train: bert or rnn')
     args = parser.parse_args()
     main(args.model)
+
